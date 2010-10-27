38a8e510ab40a173fe3abc2b1b72371f07f156bd
danielorozco87 <danielorozco87@9779c648-c10a-3698-c674-a62b956c967a>
2010-10-27 19:55:40 +0000

    Se arreglo el testCase de linearInterpolator.

--- trunk/src/tests/interpolators/LinearInterpolatorTestCase.java
+++ trunk/src/tests/interpolators/LinearInterpolatorTestCase.java
@@ -1,35 +1,80 @@
 package tests.interpolators;
 
+import static org.junit.Assert.assertEquals;
 import methods.Function;
 import methods.interpolators.LinearInterpolator;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
+@RunWith(value = Parameterized.class)
 public class LinearInterpolatorTestCase {
+	
+	private static final double ERROR = 0.000001;
 
-	//TODO: terminar!
+	private List<Point2D.Double> points;
+	private Function expectedFunction;
+	
+	public LinearInterpolatorTestCase(List<Point2D.Double> points, Function expectedFunction) {
+		this.points = points;
+		this.expectedFunction = expectedFunction;
+	}
+	
     @Test
     public void testLinearInterpolator(){
-        List<Point2D.Double> points = createPoints();
         LinearInterpolator li = new LinearInterpolator();
         Function interpolation = li.interpolate(points);
-        System.out.println(interpolation.eval(1));
-        System.out.println(interpolation.eval(2));
-        System.out.println(interpolation.eval(3));
-        System.out.println(interpolation.eval(4));
-        System.out.println(interpolation.eval(5));
-        System.out.println(interpolation.eval(6));
+        for(int i = 0; i<7; i++) {
+        	testAssertEquals(expectedFunction.eval(i), interpolation.eval(i));
+        }
+        printResult(interpolation);
+    }
+    
+    private void testAssertEquals(double expected, double actual) {
+    	assertEquals("The index founded was not the expected", expected, actual, ERROR);
+	}
+	
+    private void printResult(Function function) {
+    	for(int i = 0; i<7; i++) {
+    		System.out.println("f("+i+") = " + function.eval(i));
+    	}
     }
 
      public static List<Point2D.Double> createPoints() {
         List<Point2D.Double> points = new ArrayList<Point2D.Double>();
         points.add(new Point2D.Double(0, 0));
-        points.add(new Point2D.Double(2, 2));
-        points.add(new Point2D.Double(3, 3));
-        points.add(new Point2D.Double(4, 4));
+        points.add(new Point2D.Double(2, 4));
+        points.add(new Point2D.Double(4, 0));
+        points.add(new Point2D.Double(5, 8));
         return points;
     }
+     
+    private static Function createExpectedFunction() {
+    	return new Function() {
+    		@Override
+    		public double eval(double x) {
+    			if(x == 0) return 0;
+    			if(x == 1) return 2;
+    			if(x == 2) return 4;
+    			if(x == 3) return 2;
+    			if(x == 4) return 0;
+    			if(x == 5) return 8;
+    			if(x == 6) return 16;
+    			return 0;
+    		}
+    	};
+ 	}
+     
+     @Parameters
+     public static Collection<Object[]> data() {
+         Object[][] parameters = new Object[][] {{createPoints(), createExpectedFunction()}};    
+     	return Arrays.asList(parameters);
+     }
 }
