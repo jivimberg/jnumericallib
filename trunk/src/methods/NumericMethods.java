60939d4aad4816adc3b56ee2fab87c3c0e7f9a10
jivimberg <jivimberg@9779c648-c10a-3698-c674-a62b956c967a>
2010-11-06 16:23:36 +0000

    Ya anda Romberg y agrego Goerztel NO probado

--- trunk/src/methods/NumericMethods.java
+++ trunk/src/methods/NumericMethods.java
@@ -9,5 +9,14 @@ public class NumericMethods {
 	{
 		return Bisection.findRoot(f, a, b, error, iterations);
 	}
+	
+	public double newtonRaphson(Function f, Function derivedf, double p0, double error, int iterations) 
+		throws RootNotFoundException{
+		return NewtonRaphson.findRoot(f, derivedf, p0, error, iterations);
+	}
+	
+	public double romberg(Function f, double a, double b, int k){
+		return Romberg.romberg(f, a, b, k);
+	}
 
 }
